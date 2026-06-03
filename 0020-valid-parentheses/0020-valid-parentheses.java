ac9839d108913aa85186ddfac693c30b05a78da9
Lakshika Gangwar <161489256+Dashmaster18@users.noreply.github.com>
2026-06-03 11:41:57 +0530

    Time: 10 ms (5.78%), Space: 47.1 MB (5.74%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValid(String s) {
+        String stack = "";
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack += ch;
+            } else {
+                if (stack.length() == 0)
+                    return false;
+
+                char top = stack.charAt(stack.length() - 1);
+
+                if ((ch == ')' && top == '(') ||
+                    (ch == '}' && top == '{') ||
+                    (ch == ']' && top == '[')) {
+
+                    stack = stack.substring(0, stack.length() - 1);
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.length() == 0;
+    }
+}
