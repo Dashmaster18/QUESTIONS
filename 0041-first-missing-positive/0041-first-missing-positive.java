55ef6123141548077219288eef12b26157209f2e
Lakshika Gangwar <161489256+Dashmaster18@users.noreply.github.com>
2026-06-06 10:43:40 +0530

    Time: 17 ms (15.76%), Space: 73.8 MB (27.03%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
+
+        int missing = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == missing) {
+                missing++;
+            }
+        }
+
+        return missing;
+    }
+}
